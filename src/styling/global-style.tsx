66722814086af0fe3f6e7868a463e4820f98984f
Josh Blumberg <jlblumberg@gmail.com>
2021-03-14 10:11:57 +0000

    Update font

--- src/styling/global-style.tsx
+++ src/styling/global-style.tsx
@@ -3,6 +3,7 @@ import { createGlobalStyle } from "styled-components";
 export default createGlobalStyle`
   body {
     background-color: ${(p) => p.theme.colors.beige};
+    font-family: "Helvetica"
   }
   a { 
     text-decoration: none;
